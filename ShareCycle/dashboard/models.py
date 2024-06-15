0c8ca6a3d31f30980be3c22eec36923d0652f7bb
Alok <karnaalok48@gmail.com>
2024-06-15 19:36:09 +0545

    commit from alok

--- ShareCycle/dashboard/models.py
+++ ShareCycle/dashboard/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class User(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_profile = models.ImageField(upload_to="ShareCycle/static/image", blank=True, null=True)
+    user_contact = models.PositiveBigIntegerField()
+    user_address = models.CharField(max_length=30)
+
+class Items(models.Model):
+    category = [
+        ('Food', 'Food'),
+        ('Clothing', 'Clothing'),
+        ('Electronics', 'Electronics'),
+        ('Books', 'Books'),
+        ('Other', 'Other')
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    item_name = models.CharField(max_length=20)
+    item_exp_date = models.DateField()
+    item_collect_before = models.DateField()
+    item_description = models.CharField(max_length=50)
+    item_category = models.CharField(choices=category, max_length=15)
--- ShareCycle/dashboard/views.py
+++ ShareCycle/dashboard/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render, HttpResponse
+from django.contrib.auth.decorators import login_required
+from .models import User, Items
 
 # Create your views here.
 
 def index(request):
         return render(request, 'welcome.html')
 
+
+@login_required
 def dashboard(request):
+    user = request.user
+    inds = User.objects.filter(user = user)
+    ind_list = []
+    for ind in inds:
+        contact = ind.user_contact
+        ind_list.append(contact)
+    print(ind_list)
+    
     return render(request, 'dashboard/home.html')
